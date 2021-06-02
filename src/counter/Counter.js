dbc55e97218d2c91f86c3e776bbc63640d08d68f
DePrincio <princetlive@yahoo.com>
2021-06-02 13:18:23 -0400

    add testing for buttons and input

--- src/counter/Counter.js
+++ src/counter/Counter.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Counter() {
+	const [counterValue, setCounterValue] = useState(1);
+	const [inputValue, setInputValue] = useState(1);
 	return (
 		<div>
-			<h1>Couter</h1>
+			<h1 data-testid='header'>Counter</h1>
+			<h1 data-testid='counter'>{counterValue}</h1>
+			<button data-testid='add-button'>+</button>
+			<input data-testid='input' type='number' value={inputValue} />
+			<button data-testid='subtract-button'>-</button>
 		</div>
 	);
 }
--- src/counter/Counter.test.js
+++ src/counter/Counter.test.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import Counter from '../counter/Counter';
-import { render } from '@testing-library/react';
+import { getByTestId, render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 test('header renders with correct text', () => {
-	const component = render(<Counter />);
+	// const component = render(<Counter />);
+	// const headerEl = component.getByTestId('header');
+
+	//second method -> destructure the getByTestId from render
+	const { getByTestId } = render(<Counter />);
+	const headerEl = getByTestId('header');
+
+	expect(headerEl.textContent).toBe('Counter');
+});
+
+test('counter initially start with text of 1', () => {
+	const { getByTestId } = render(<Counter />);
+	const counterEl = getByTestId('counter');
+	expect(counterEl.textContent).toBe('1');
+});
+
+test('add button renders with +', () => {
+	const { getByTestId } = render(<Counter />);
+	const counterEl = getByTestId('add-button');
+	expect(counterEl.textContent).toBe('+');
+});
+test('subtract button renders with -', () => {
+	const { getByTestId } = render(<Counter />);
+	const counterEl = getByTestId('subtract-button');
+	expect(counterEl.textContent).toBe('-');
+});
+test('input countains 1 initially', () => {
+	const { getByTestId } = render(<Counter />);
+	const counterEl = getByTestId('input');
+	expect(counterEl.textContent).toBe(1);
 });
